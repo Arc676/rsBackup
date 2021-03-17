23ae42b0dfabb1efce3e458ef623efc2c3a8fef1
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2021-03-17 22:38:50 +0100

    Add struct for parsing command line options

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,44 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-use std::io;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "rsbackup", about = "rsync backup utility written in Rust")]
+struct Options {
+	#[structopt(short = "f", long = "conf", parse(from_os_str))]
+	config: Option<PathBuf>,
+
+	#[structopt(long)]
+	always_confirm: bool,
+
+	#[structopt(long)]
+	ask: bool,
+
+	#[structopt(long)]
+	debug: bool,
+
+	#[structopt(long)]
+	link: bool,
+
+	#[structopt(short, long)]
+	quiet: bool,
+
+	#[structopt(long)]
+	up_only: bool,
+
+	#[structopt(long)]
+	id_tasks: bool,
+
+	#[structopt(short = "s", long = "safe")]
+	quit_on_fail: bool,
+
+	#[structopt(long)]
+	download: bool
+}
+
 fn main() {
-    println!("Hello, world!");
+	let opt = Options::from_args();
+	println!("{:?}", opt);
 }
